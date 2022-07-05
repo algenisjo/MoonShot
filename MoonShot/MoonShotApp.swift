bd21a9a058eac2bd110cac82c000addd97d9a5cf
Algenis Joaquin Ortiz <71911185+algenisjo@users.noreply.github.com>
2022-07-05 09:29:33 -0400

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Algenis Joaquin Ortiz on 7/5/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
